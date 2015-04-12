2237f85583028cf57af9981d4d9da14357238237
Johnny Wang <johnnywang@gmail.com>
2015-04-12 18:46:06 -0400

    Added subscriber so vision system will not run until it sees 'true' in 'start_vision' topic. It can also shut off when it sees 'false' again. Also removed debug prints.

--- ROS/src/detect_can/src/detect_cylinder.cpp
+++ ROS/src/detect_can/src/detect_cylinder.cpp
@@ -20,6 +20,7 @@
 #include <pcl/segmentation/extract_clusters.h>
 
 #include <geometry_msgs/Vector3.h>
+#include <std_msgs/Bool.h>
 
 #include <typeinfo>
 
@@ -29,18 +30,25 @@ ros::Publisher pub_coord;
 ros::Publisher pub_plane_seg;
 ros::Publisher pub_temp;
 
+// To decide if we should start running vision system
+bool g_run_vision = false;
+
 // For calculating rate of cylinder count
-int not_cylinder = 0;
-int cylinder = 0;
-double start_time = 0;
-double not_cyl_frac = 0;
-double cyl_frac = 0;
+int g_not_cylinder = 0;
+int g_cylinder = 0;
+double g_start_time = 0;
+double g_not_cyl_frac = 0;
+double g_cyl_frac = 0;
 
 typedef pcl::PointXYZ PointT;
 
 void 
 cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 {
+	// Return if we're not supposed to start running vision system yet
+	if (!g_run_vision)
+		return;
+
 	//pcl::PCLPointCloud2::Ptr cloud (new pcl::PCLPointCloud2);
 	pcl::PointCloud<PointT>::Ptr cloud(new pcl::PointCloud<PointT>);
 
@@ -52,15 +60,11 @@ cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 	pcl::PointCloud<PointT>::Ptr cloud_v (new pcl::PointCloud<PointT>);
 	pcl::PointCloud<PointT>::Ptr cloud_f (new pcl::PointCloud<PointT>);
 	pcl::PointCloud<PointT>::Ptr cloud_in (new pcl::PointCloud<PointT>);
-	//pcl::PCLPointCloud2::Ptr cloud_v (new pcl::PCLPointCloud2);
-	//pcl::PCLPointCloud2::Ptr cloud_f (new pcl::PCLPointCloud2);
-	//pcl::PCLPointCloud2::Ptr cloud_in (new pcl::PCLPointCloud2);
-	//pcl::PassThrough<pcl::PCLPointCloud2> pass;
+
 	pcl::PassThrough<PointT> pass;
 	pcl::ExtractIndices<PointT> extract;
 	pcl::SACSegmentation<PointT> seg_plane;
 	pcl::SACSegmentationFromNormals<PointT, pcl::Normal> seg_cyl;
-	//pcl::VoxelGrid<pcl::PCLPointCloud2> sor2;
 	pcl::VoxelGrid<PointT> sor2;
 
 	pcl::NormalEstimation<PointT, pcl::Normal> ne;
@@ -110,19 +114,10 @@ cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 //    sor1.setStddevMulThresh (0.001);  //1  0.5
 //    sor1.filter(*cloud_v);
 
-//	// ESTIMATE POINT NORMALS
-//	ne.setSearchMethod (tree);
-//	ne.setInputCloud (cloud_v);
-//	ne.setKSearch (50);
-//	ne.compute (*cloud_normals);
-
 	// EXTRACT PLANES FROM IMAGE
 	pcl::PointCloud<PointT>::Ptr inPCseg (new pcl::PointCloud<PointT>);
 	pcl::PointCloud<PointT>::Ptr outPCseg (new pcl::PointCloud<PointT>);
-	//pcl::fromPCLPointCloud2 (*cloud_v,*inPCseg);
 	*inPCseg = *cloud_v;
-	//pcl::fromPCLPointCloud2 (*cloud,*inPCseg);
-	//pcl::fromPCLPointCloud2 (*cloud_in,*inPCseg);
 
 	pcl::ModelCoefficients::Ptr coefficients_plane (new pcl::ModelCoefficients);
 	pcl::PointIndices::Ptr inliers_plane (new pcl::PointIndices);
@@ -134,7 +129,7 @@ cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 	seg_plane.setMaxIterations (100);
 	seg_plane.setDistanceThreshold (0.007);  // 0.01 good but some artifacts
 
-std::cerr << "Points: " << inPCseg->points.size() << std::endl;
+//std::cerr << "Points: " << inPCseg->points.size() << std::endl;
 
 	int i = 0, count = 0, nr_points = (int) inPCseg->points.size();
 	// Keep segmenting planes until 10% of points are left
@@ -164,8 +159,8 @@ std::cerr << "Points: " << inPCseg->points.size() << std::endl;
 //        extract_normals.setIndices (inliers_plane);
 //        extract_normals.filter (*cloud_normals);
 
-		std::cerr << "Planar component: " << outPCseg->width * outPCseg->height 
-		<< " data points." << std::endl;
+		//std::cerr << "Planar component: " << outPCseg->width * outPCseg->height 
+		//<< " data points." << std::endl;
 
 		inPCseg.swap(outPCseg);
 		count++;
@@ -212,22 +207,8 @@ std::cerr << "Points: " << inPCseg->points.size() << std::endl;
 	ec.setInputCloud (outPCseg);
 	ec.extract (cluster_indices);
 
-	std::cerr << "Cluster size: " << cluster_indices.size() << std::endl;
+	//std::cerr << "Cluster size: " << cluster_indices.size() << std::endl;
 	if (cluster_indices.size() > 0) {
-		// Print out center coordinate of the first cluster we have
-		//std::cerr << "------------------------------------------------------------" << std::endl;
-		//for (int i = 0; i < cluster_indices.size(); i++) {
-		//	Eigen::Vector4f centroid;
-		//	//pcl::compute3DCentroid(*outPCseg, cluster_indices[0], centroid);
-		//	pcl::compute3DCentroid(*outPCseg, cluster_indices[i], centroid);
-		//	std::cerr << centroid[0] << " " << centroid[1] << " " << centroid[2] << std::endl;
-		//	geometry_msgs::Vector3 can_coord;
-		//	can_coord.x = centroid[0];
-		//	can_coord.y = centroid[1];
-		//	can_coord.z = centroid[2];
-		//}
-
-
 		/*
 		 * To separate each cluster out of the vector<PointIndices> we have to iterate through
 		 * cluster_indices, create a new PointCloud for each entry and write all points of the
@@ -285,13 +266,13 @@ std::cerr << "Points: " << inPCseg->points.size() << std::endl;
 			float point_threshold = 0.5;
 			if (cloud_cylinder->points.size () <= point_threshold * cloud_cluster->points.size()) {
 				std::cerr << "NOT CYLINDER" << std::endl;
-				not_cylinder++;
-				not_cyl_frac += seg_frac;
+				g_not_cylinder++;
+				g_not_cyl_frac += seg_frac;
 			} else {
 				std::cerr << "CYLINDER" << std::endl;
 				*clustered_cloud += *cloud_cylinder;
-				cylinder++;
-				cyl_frac += seg_frac;
+				g_cylinder++;
+				g_cyl_frac += seg_frac;
 
 				// Output centroid coordinate
 				Eigen::Vector4f centroid;
@@ -305,92 +286,42 @@ std::cerr << "Points: " << inPCseg->points.size() << std::endl;
 				pub_coord.publish(can_coord);
 
 			}
-			double now_time = ros::Time::now().toSec() - start_time;
+			double now_time = ros::Time::now().toSec() - g_start_time;
 			ROS_INFO("Time: %f\n", now_time);
 
-			ROS_INFO("Not Cylinder/second: %f\n", not_cylinder/now_time);
-			ROS_INFO("Avg Not Cylinder fraction: %f\n", not_cyl_frac/not_cylinder);
-			ROS_INFO("Percent Not Cylinder: %f\n", (float)not_cylinder/(float)(cylinder+not_cylinder));
+			ROS_INFO("Not Cylinder/second: %f\n", g_not_cylinder/now_time);
+			ROS_INFO("Avg Not Cylinder fraction: %f\n", g_not_cyl_frac/g_not_cylinder);
+			ROS_INFO("Percent Not Cylinder: %f\n", (float)g_not_cylinder/(float)(g_cylinder+g_not_cylinder));
 
-			ROS_INFO("Cylinder/second: %f\n", cylinder/now_time);
-			ROS_INFO("Avg Cylinder fraction: %f\n", cyl_frac/cylinder);
-			ROS_INFO("Percent Cylinder: %f\n", (float)cylinder/(float)(cylinder+not_cylinder));
+			ROS_INFO("Cylinder/second: %f\n", g_cylinder/now_time);
+			ROS_INFO("Avg Cylinder fraction: %f\n", g_cyl_frac/g_cylinder);
+			ROS_INFO("Percent Cylinder: %f\n", (float)g_cylinder/(float)(g_cylinder+g_not_cylinder));
 
 		}
 
 		// Pass to "test_cloud" topic to debug
 		// Convert to ROS
 		pcl::toPCLPointCloud2(*cloud_normals, *object);  // PointXYZ to PointCloud2
-		//pcl::toPCLPointCloud2(*clustered_cloud, *object);  // PointXYZ to PointCloud2
-    	//pcl::toPCLPointCloud2(*outPCseg, *object);  // PointXYZ to PointCloud2
     	pcl_conversions::fromPCL(*object, output); // PointCloud2 to sensor-msgs
     	output.header.frame_id = input->header.frame_id;
     	pub_temp.publish(output);
 
-	/*
-	// print cluster point cloud extract.setInputCloud (outPCseg);
-	extract.setIndices (cluster_indices[0]);
-	extract.setNegative (true);
-	extract.filter (*cloud_v);
-	pcl::toPCLPointCloud2(*cloud_v, *object);  // PointXYZ to PointCloud2
-	pcl_conversions::fromPCL(*object, output); // PointCloud2 to sensor-msgs
-	output.header.frame_id = input->header.frame_id;
-	
-	pub_temp.publish(output);
-	*/
 	}
 
-//for (int i=0; i < cluster_indices.size(); i++) {
-
-/*
-	// ESTIMATE POINT NORMALS
-	ne.setSearchMethod (tree);
-	ne.setInputCloud (outPCseg);
-	//ne.setKSearch (50);
-	ne.setRadiusSearch (0.1);
-	ne.compute (*cloud_normals);
-
-	// SEGMENTATION FOR CYLINDER OBJECT
-	seg_cyl.setOptimizeCoefficients (true);
-	seg_cyl.setModelType (pcl::SACMODEL_CYLINDER);
-	seg_cyl.setMethodType (pcl::SAC_RANSAC);
-	seg_cyl.setMaxIterations (100);
-	seg_cyl.setDistanceThreshold (0.01);
-	seg_cyl.setRadiusLimits (0.03, 0.06);
-	seg_cyl.setInputCloud (outPCseg);
-	seg_cyl.setNormalDistanceWeight (0);
-	seg_cyl.setInputNormals (cloud_normals);
-
-	// OBTAIN CYLINDER INLIERS AND COEFFICIENTS
-	seg_cyl.segment (*inliers_cylinder, *coefficients_cylinder);
-	std::cerr << "Cylinder coefficients: " << *coefficients_cylinder << std::endl;
-
-	extract.setInputCloud (outPCseg);
-	extract.setIndices (inliers_cylinder);
-	extract.setNegative (false);
-	extract.filter (*cloud_cylinder);
-	if (cloud_cylinder->points.empty ()) {
-		std::cerr << "NO CYLINDER" << std::endl;
-	// return?
-	} else {
-		std::cout << "CYLINDER" << std::endl;
-	}
-	// Pass to "test_cloud" topic to debug
-    // Convert to ROS
-    pcl::toPCLPointCloud2(*outPCseg, *object);  // PointXYZ to PointCloud2
-    pcl_conversions::fromPCL(*object, output); // PointCloud2 to sensor-msgs
-    output.header.frame_id = input->header.frame_id;
-    pub_temp.publish(output);
-*/
 	// Convert to ROS data type
-	//pcl::toPCLPointCloud2(*outPCseg,*cloud_f);
-	//pcl::toPCLPointCloud2(*cloud_cylinder,*cloud_f);
 	pcl::toPCLPointCloud2(*cloud_cylinder, *object);  // PointXYZ to PointCloud2
-	//pcl::toPCLPointCloud2(*outPCseg, *object);  // PointXYZ to PointCloud2
 	pcl_conversions::fromPCL(*object, output); // PointCloud2 to sensor-msgs
 	output.header.frame_id = input->header.frame_id;
 	
 	pub_cloud.publish(output);
+}
+
+void 
+vision_cb (const std_msgs::Bool input) {
+	if (input.data) 
+		g_run_vision = true;
+	else
+		g_run_vision = false;
 }
 
 int
@@ -400,12 +331,14 @@ main (int argc, char** argv)
 	ros::init (argc, argv, "detect_can");
 	ros::NodeHandle nh;
 
-	start_time = ros::Time::now().toSec();
-	
+	g_start_time = ros::Time::now().toSec();
+
+	// Create subscriber to check if we should start running
+	ros::Subscriber start_vision = nh.subscribe("start_vision", 1, vision_cb);
+
 	// Create a ROS subscriber for the input point cloud
 	ros::Subscriber sub = nh.subscribe ("/camera/depth/points", 1, cloud_cb);
-	//ros::Subscriber sub = nh.subscribe ("/camera/rgb/points", 1, cloud_cb);
-	
+		
 	// Create a ROS publisher for the output cloud
 	pub_cloud = nh.advertise<sensor_msgs::PointCloud2> ("detect_can_cloud", 1);
 	// Create a ROS publisher for cluster center
