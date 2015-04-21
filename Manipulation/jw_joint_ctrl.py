89cfc443920281880ed519dbda2771ec1c8fe9c7
Johnny Wang <johnnywang@gmail.com>
2015-04-21 16:17:38 -0400

    Updated home position so it's not as close to the body. Also added flag so arm will always move to an area where it won't hit the base while moving to bottle. Updated copyright.

--- Manipulation/jw_joint_ctrl.py
+++ Manipulation/jw_joint_ctrl.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python
+
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2015, Johnny Wang
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of SRI International nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Johnny Wang
+
 import roslib
 #roslib.load_manifest('my_dynamixel_tutorial')
 
@@ -22,17 +57,25 @@ class Joint:
         rospy.Subscriber("shoulder_pan_joint/state", JointState, self.callback)
        
         rospy.sleep(1)  # so we have some time to get pan_angle from the callback
- 
-        self.move_home()
+
+        self.at_home = False
+        self.move_home()   # self.at_home will be "True" after this call
 
     def move_joint(self, angles):
-        goal = FollowJointTrajectoryGoal()                      
+
+        # If arm is at home, move it up first to move out of the way of the base
+        if self.at_home:
+            self.move_joint([self.pan_angle, -0.5, 1.7, 0, -1])
+
+        goal = FollowJointTrajectoryGoal()
         goal.trajectory.joint_names = ['shoulder_pan_joint', 'shoulder_pitch_joint','elbow_flex_joint','wrist_roll_joint','gripper_joint']
         point = JointTrajectoryPoint()
         point.positions = angles
         point.time_from_start = rospy.Duration(3)                       
         goal.trajectory.points.append(point)
         self.jta.send_goal_and_wait(goal)
+        
+        self.at_home = False   # arm is no longer at home position
 
     def callback(self, data):
         self.pan_angle = data.current_pos
@@ -49,10 +92,11 @@ class Joint:
         # Move elbow_flex up to level first
         self.move_joint([self.pan_angle, -0.5, 1.7, 0, -1])
         # Move should_pan to the side
-        self.move_joint([-1.4, -0.5, 1.7, 0, -1])
+        self.move_joint([-1.5, -0.5, 1.7, 0, -1])
         # Move elbow down to default position
-        self.move_joint([-1.4, -1.4, 1.7, 0, -1])
-        
+        self.move_joint([-1.5, -1.3, 1.7, 0, -1])
+       
+        self.at_home = True 
 
 def main():
         arm = Joint()
