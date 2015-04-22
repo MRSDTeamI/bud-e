13875c3ef30f86c1a0ed227bef68f4af7a074124
Johnny Wang <johnnywang@gmail.com>
2015-04-22 09:47:06 -0400

    New offsets.

--- ROS/src/detect_can/src/parse_center.cpp
+++ ROS/src/detect_can/src/parse_center.cpp
@@ -84,9 +84,9 @@ std::cerr << "----------------" << std::endl;
 				double bottle_ht = 0.088;   // approx. bottle center height (above gnd plane)
                 double kinect_x_coeff = 0; // For some reason, it keeps reporting objects a little bit offset.
 				
-				arm_output.x = z - kinect_depth -0.02;
-				arm_output.y = -1*(x - kinect_x_coeff) + 0.1;
-				arm_output.z = -1*(y - kinect_height) + 0.06;
+				arm_output.x = z - kinect_depth - 0.02;
+				arm_output.y = -1*(x - kinect_x_coeff) + 0.09;
+				arm_output.z = -1*(y - kinect_height) + 0.07;
 				pub_bot.publish(arm_output);
 
 				// for scissor lift
@@ -109,7 +109,8 @@ std::cerr << "----------------" << std::endl;
 		case MONITOR:
 			// Count how many times in a row our y displacement is greater
 			// than 2.0 cm
-			if (fabs(y-input.y) > height_dev) {
+			if ((fabs(y-input.y) > height_dev) || (fabs(x-input.x) > height_dev)
+                || (fabs(z-input.z) > height_dev)) {
 				reset_count++;
 			} else {
 				reset_count = 0;
