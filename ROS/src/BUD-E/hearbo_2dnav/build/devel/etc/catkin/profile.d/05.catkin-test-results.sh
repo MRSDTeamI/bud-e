52646e3ec846ee448e24653aaa6d3ba2f8c78bad
Johnny Wang <johnnywang@gmail.com>
2015-04-22 03:03:55 -0400

    Updated parse_center so it resets based on reset_parse topic. Updated InverseKin.py so it uses reset_parse to tell parse_center to start over and get new bottle coordinates.

--- ROS/src/BUD-E/hearbo_2dnav/build/devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
-
-export CATKIN_TEST_RESULTS_DIR="/home/praveen/Documents/ROS/tmp/hearbo_2dnav/build/test_results"
-export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
